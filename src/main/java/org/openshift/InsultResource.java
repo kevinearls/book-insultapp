86c7e19035b5add9597e75ac18728d02a9096b7b
Kevin Earls <kevin@kevinearls.com>
2017-05-16 16:53:22 +0200

    Added tracing

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -1,5 +1,16 @@
 package org.openshift;
 
+import com.uber.jaeger.Tracer;
+import com.uber.jaeger.metrics.Metrics;
+import com.uber.jaeger.metrics.NullStatsReporter;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.reporters.Reporter;
+import com.uber.jaeger.samplers.ConstSampler;
+import com.uber.jaeger.samplers.Sampler;
+import com.uber.jaeger.senders.Sender;
+import com.uber.jaeger.senders.UDPSender;
+import io.opentracing.Span;
+
 import java.util.HashMap;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
@@ -10,11 +21,25 @@ import javax.ws.rs.Produces;
 @Path("/insult")
 public class InsultResource {
 
+    private static io.opentracing.Tracer tracer = initTracer("Insults");
+
     @GET()
     @Produces("application/json")
     public HashMap<String, String> getInsult() {
         HashMap<String, String> theInsult = new HashMap<String, String>();
-        theInsult.put("insult", new InsultGenerator().generateInsult());
+        String insult =  new InsultGenerator().generateInsult();
+        theInsult.put("insult", insult);
+
+        Span span = tracer.buildSpan("generateInsult").withTag("insult", insult).start();
+        span.finish();
         return theInsult;
+    }
+
+    public static io.opentracing.Tracer initTracer(String serviceName) {
+        Sampler sampler = new ConstSampler(true);
+        Sender sender = new UDPSender(null, 0, 0);
+        Reporter reporter = new RemoteReporter(sender, 500, 1000, Metrics.fromStatsReporter(new NullStatsReporter()));
+        Tracer tracer = new Tracer.Builder(serviceName, reporter, sampler).build();
+        return tracer;
     }
 }
